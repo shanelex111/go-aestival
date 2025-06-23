34d83a2243e2cabce504af51127e75b32bb99af6
xinyu.zhang <zhang.li@net263.com>
2025-06-23 18:02:45 +0800

    update signout

--- go-auth/cmd/router/router.go
+++ go-auth/cmd/router/router.go
@@ -19,7 +19,7 @@ func Run() {
 	{
 		authGroup.POST("/signin", auth.Signin)
 		authGroup.POST("/refresh-token", auth.RefreshToken)
-		authGroup.DELETE("/signout", request.TokenAuth(), auth.Signout)
+		authGroup.DELETE("/signout", request.AuthAccessToken(), auth.Signout)
 		authGroup.DELETE("/account", auth.DeleteAccount)
 
 		authGroup.PUT("/password", auth.ResetPassword)
